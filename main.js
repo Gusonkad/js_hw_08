e2b0633e1e5b1183f833b6826885189f6694ebf3
Dima Gusonka <gusonkad@gmail.com>
2021-05-12 00:55:05 +0300

    Add some changes to main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Gallery
 "use strict";
 
 import gallery from "./gallery-items.js";
@@ -13,7 +14,7 @@ const refs = {
     lightboxContent: document.querySelector(".lightbox__content"),
     lightBoxBtn: document.querySelector(".lightbox__button")
 };
-
+// Add elements
 const galleryProcess = gallery => {
     return gallery.map(({ preview, original, description }) => {
         const localRefs = {
@@ -23,7 +24,6 @@ const galleryProcess = gallery => {
             span: document.createElement("span"),
             i: document.createElement("i")
         };
-
         localRefs.item.classList.add("gallery__item");
         localRefs.link.classList.add("gallery__link");
         localRefs.link.setAttribute("href", original);
@@ -37,72 +37,76 @@ const galleryProcess = gallery => {
         localRefs.link.appendChild(localRefs.image);
         localRefs.link.appendChild(localRefs.span);
         localRefs.item.appendChild(localRefs.link);
-
         return localRefs.item;
     });
 };
-
+// Show all small images
 const items = galleryProcess(gallery);
-items.map(item => {
+items.forEach(item => {
     refs.list.appendChild(item);
 });
-
+// Open big image
 const handleClick = e => {
-
     e.preventDefault();
     const data = e.target.dataset.source;
-    const image = refs.lightboxImage.cloneNode(false);
+    let image = refs.lightboxImage.cloneNode(false);
     image.setAttribute("src", data);
     image.setAttribute("alt", e.target.alt);
     refs.lightboxContent.innerHTML = "";
     refs.lightboxContent.append(image);
     refs.Lightbox.classList.add("is-open");
-
 };
-
+// Close big image
 const closeHandler = () => {
     refs.Lightbox.classList.remove("is-open");
 };
-
+// Escape button
 const handleKeyup = e => {
     if (e.keyCode == KEYCODE_ESC) {
         console.log("key ESC pressed");
         closeHandler();
     }
-    // Image left, right 
+// Image left, right 
+    let img =  image.setAttribute("src", data);
+    let allItems = items;
+
+    // Right
     if (
         e.keyCode == KEYCODE_right && refs.Lightbox.classList.contains("is-open")
     ) {
-        let index = items.indexOf(link.src);
-        if (index != items.length - 1) {
+        let index = allItems.indexOf(img);
+        if (index != allItems.length - 1) {
             index++;
-            link.src = items[index];
+            img = allItems[index];
         }
         console.log(index);
     }
-
+// left
     if (
         e.keyCode == KEYCODE_left && refs.Lightbox.classList.contains("is-open")
     ) {
-        let idx = items.indexOf(link.src);
-        if (idx <= -1 || idx === 0 || idx == link.length - 1) {
-            idx == items.length - 1;
-            link.src = items[idx];
+        let index = allItems.indexOf(img);
+        if (index <= -1 || index === 0 || index == allItems.length - 1) {
+            index == allItems.length - 1;
+            img = allItems[index];
         } else {
-            link.src = items[--idx];
+            img = allItems[--index];
         }
+        console.log(index);
     }
 };
-
+// Click overlay
 const contentClickHandler = e => {
     if (e.target !== e.currentTarget) {
-        return closeHandler();
+        closeHandler();
     }
-
 };
-
-
+// Listenrs
 refs.list.addEventListener("click", handleClick);
 refs.lightBoxBtn.addEventListener("click", closeHandler);
 window.addEventListener("keyup", handleKeyup);
 refs.lightboxContent.addEventListener("click", contentClickHandler);
+
+
+
+
